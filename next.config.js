5705943011261143cfe793caf9be16243b865a89
EulerPascal404 <67972733+EulerPascal404@users.noreply.github.com>
2025-04-13 23:24:34 -0700

    Update CORS for Vercel deployment

--- next.config.js
+++ next.config.js
@@ -3,7 +3,10 @@ const nextConfig = {
     reactStrictMode: true,
     swcMinify: true,
     images: {
-        domains: ['prometheus-ai-backend.herokuapp.com'],
+        domains: [
+            'prometheus-ai-backend.herokuapp.com',
+            'prometheus-ai-backend-app-589cbe98fdc3.herokuapp.com'
+        ],
     },
     env: {
         NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
